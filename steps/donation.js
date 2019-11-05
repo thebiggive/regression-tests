6beea2ca02dd76e25c23299c78e621b4d41d4056
Mohammed Mahdy <mohamed.mahdy@camelcasetech.com>
2019-11-05 15:12:08 +0200

    fix lint errors

--- steps/donation.js
+++ steps/donation.js
@@ -1,7 +1,6 @@
 import { Given, When, Then } from 'cucumber';
 import {
-    goToUrl, clickSelector, inputSelectorValue, sendKeys, checkUrl, checkTitle,
-    checkSelectorContent
+    goToUrl
 } from '../support/common';
 
 Given(
@@ -13,77 +12,77 @@ Given(
 
 When(
     /^I enter an amount between £5 and £25,000$/,
-    (firstAmount, secondAmount) => {
+    () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I choose a preference for Gift Aid, charity comms and TBG comms$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I press Donate$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^Then I am taken to Charity Checkout pages, where I can complete my other donation information$/,
     () => {
         // TODO
     }
-)
+);
 
 When(
     /^my bank approves the charge and the payment steps took less than 15 minutes$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I should be redirected to a Thank You confirmation page$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I should see an initial message saying the donation succeeded$/,
     () => {
         // TODO
     }
-)
+);
 
 When(
     /^I wait 5 seconds$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I should see my Charity Checkout transaction ID$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I should see my matched amount is the same as my donation amount$/,
     () => {
         // TODO
     }
-)
+);
 
 Then(
     /^I should see the total value of my donation is double my donation amount, plus any Gift Aid$/,
     () => {
         // TODO
     }
-)
+);
