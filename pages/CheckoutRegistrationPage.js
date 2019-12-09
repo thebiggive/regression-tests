8a69123a14960007b8f017adaf126db8d7b4bf71
John Levermore <john.levermore@zhibek.com>
2019-12-09 18:13:35 +0000

    Permit fixed wait for reduced set of situations - e.g. on checkout registration.

--- pages/CheckoutRegistrationPage.js
+++ pages/CheckoutRegistrationPage.js
@@ -46,6 +46,7 @@ export default class CheckoutRegistrationPage {
         checkUrlMatch(urlCheck);
         checkTitle(titleCheck);
         checkSelectorContent(headingSelector, pageHeadingCheck);
+        wait(10);
         checkSelectorContent(subHeadingSelector, pageSubHeadingCheck);
     }
 
