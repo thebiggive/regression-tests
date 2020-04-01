726cd2b3956286ef9280de2a3f95d540a3827e26
John Levermore <john.levermore@zhibek.com>
2020-04-01 19:19:55 +0100

    Move webhook logic into separate "checkout webhook" page object. Ensure we check for "checkout success" *before* webhook to make sure URL has changed to thanks/<token>.

---
+++ pages/CheckoutWebhookPage.js
@@ -0,0 +1,43 @@
+import sendCheckoutWebhook from '../support/checkout/webhook';
+
+/**
+ * checkout webhook page object
+ */
+export default class CheckoutWebhookPage {
+    /**
+     * trigger webhook
+     * @param {int} amount donation amount
+     * @param {string} firstName donor first name
+     * @param {string} lastName donor last name
+     * @param {string} guestEmail donor email
+     * @param {string} address donor address
+     * @param {string} country donor country
+     */
+    static triggerWebhook(amount, firstName, lastName, guestEmail, address,
+        country) {
+        console.log(`Donation Check URL: ${browser.getUrl()}`);
+
+        const donationID = browser.getUrl()
+            .split('/')[4]; // Donation ID
+        browser.call(async () => {
+            await sendCheckoutWebhook(
+                donationID,
+                {
+                    charityId: process.env.CHECKOUT_CHARITY_ID,
+                    projectId: process.env.CHECKOUT_PROJECT_ID,
+                    donationAmount: amount,
+                    donationMatched: false,
+                    giftAid: false,
+                    optInTbgEmail: false,
+                    firstName,
+                    lastName,
+                    emailAddress: guestEmail,
+                    billingPostalAddress: address,
+                    // using slice to remove string: text
+                    countryCode: country.slice(7),
+                    status: 'Collected',
+                }
+            );
+        });
+    }
+}
