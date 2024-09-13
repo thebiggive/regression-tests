cd5ca9c2e4cedcb133ec732ca54d09ebb7e533ee
Barney Laurance <barney@thebiggive.org.uk>
2024-09-13 15:21:29 +0100

    Revert "DON-896: Enter billing postcode even when using existing card"
    This reverts commit 0f409bd634f94e4a9698c39bbc3a033f4f05efdf.
    
    No longer needed (or possible) since
     https://github.com/thebiggive/donate-frontend/pull/1647

--- steps/donation.js
+++ steps/donation.js
@@ -7,10 +7,9 @@ import {
 
 import { checkAnEmailBodyContainsText, checkAnEmailSubjectContainsText } from '../support/mailtrap';
 import { randomIntFromInterval } from '../support/util';
-import DonateStartPage, { billingPostcodeSelector } from '../pages/DonateStartPage';
+import DonateStartPage from '../pages/DonateStartPage';
 import DonateSuccessPage from '../pages/DonateSuccessPage';
 import { checkVisibleSelectorContent } from '../support/check';
-import { inputSelectorValue } from '../support/action';
 
 /**
  * Note: donationAmount is changable in the `restart-donation` test, whereby the bot changes the
@@ -318,6 +317,3 @@ Then(
         }
     }
 );
-Then(/^I enter a billing postcode$/, async () => {
-    await inputSelectorValue(billingPostcodeSelector, 'N1 1AA');
-});
