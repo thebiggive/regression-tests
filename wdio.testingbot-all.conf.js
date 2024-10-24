f1ff6d8e6c1209e516a119581efc65924234ec80
Dorota Hawro <dorota@thebiggive.org.uk>
2024-10-24 17:21:18 +0100

    REG-37: revert ecmaVersion back to 8 and use slightly newer version of safari

--- wdio.testingbot-all.conf.js
+++ wdio.testingbot-all.conf.js
@@ -10,7 +10,7 @@ config.capabilities = [
     {
         browserName: 'safari',
         platform: 'SIERRA',
-        version: '11',
+        version: '12',
         build: config.build,
     },
     {
