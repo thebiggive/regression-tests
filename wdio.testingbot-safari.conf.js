1990704240182e51956009e1b4fc166b6c8434ae
Noel Light-Hilary <noel@noellh.com>
2025-02-18 15:27:53 +0000

    Update `chromedriver` and add script `test:testingbot-safari`

---
+++ wdio.testingbot-safari.conf.js
@@ -0,0 +1,22 @@
+const { config } = require('./wdio.BASE.conf.ts');
+
+config.capabilities = [{
+    browserName: 'safari',
+    platform: 'VENTURA',
+    version: '16',
+    build: config.build,
+}];
+config.services.push('testingbot');
+
+config.path = '/wd/hub';
+config.user = process.env.TESTINGBOT_KEY;
+config.key = process.env.TESTINGBOT_SECRET;
+
+config.reporters.push(['junit', {
+    outputDir: './build/wdio',
+    outputFileFormat(options) {
+        return `results-${options.cid}.${options.capabilities}.xml`;
+    },
+}]);
+
+exports.config = config;
