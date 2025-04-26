107a9975c7ad50f68b1cf54534a8d0b873ce75ce
Barney Laurance <barney@thebiggive.org.uk>
2025-04-26 15:17:01 +0100

    Revert "Remove unused function"
    This reverts commit 28b1964daf000b2b77f2c799c3e616dd5b9aa749.
    
    Wasn't actually unused

--- pages/DonateStartPage.js
+++ pages/DonateStartPage.js
@@ -4,7 +4,8 @@ import { generateIdentifier, goToUrl } from '../support/util';
 import {
     checkTitle,
     checkSelectorContent,
-    elementExists
+    elementExists,
+    checkVisibleSelectorContent
 }
     from '../support/check';
 import {
@@ -22,6 +23,7 @@ const startPageStripe = /** @type {string} */ (process.env.DONATE_PAGE_STRIPE);
 const regularGivingCampaignId = /** @type {string} */ (process.env.REGULAR_GIVING_CAMPAIGN_ID);
 
 // selectors
+const idInfoSelector = '.id-info';
 const submitBtnSelector = 'button*=Donate ';
 const donationAmountSelector = '#donationAmount';
 const yourDonationStepSelector = '#cdk-stepper-0-label-0';
@@ -108,6 +110,18 @@ export default class DonateStartPage {
     async checkReady() {
         await checkTitle(`Donate to ${CHARITY_NAME}`);
         await checkSelectorContent('form', CHARITY_NAME);
+    }
+
+    /**
+     * Check the Identity service info box above the stepper contains
+     * the given text.
+     *
+     * @param {string} expectedText  Text anticipated somewhere in the box.
+     */
+    async checkIdInfo(expectedText) {
+        // eslint-disable-next-line wdio/no-pause
+        await browser.pause(1500); // Give the login call a short processing time.
+        await checkVisibleSelectorContent(idInfoSelector, expectedText);
     }
 
     /**
