8e9ceffe4197c3f3cefe78477dd5cbc4224d4db8
Barney Laurance <barney@thebiggive.org.uk>
2025-09-30 17:39:44 +0100

    DON-1156: Check homepage accessibility

---
+++ features/read-home-page.feature
@@ -0,0 +1,8 @@
+Feature: Read the homepage
+
+    As someone interested in understand Big Give, I should be able to read the homepage.
+
+    Scenario: Reading the homepage
+      Given that I am on my chosen charity's Donate page
+      Then there should be no accessibility violations detected
+
---
+++ pages/HomePage.ts
@@ -0,0 +1,18 @@
+import {Given, Then} from "@cucumber/cucumber";
+import {goToUrl} from "../support/util";
+import checkNoAccessibilityViolations from "../support/a11y";
+
+Given(
+    /^that I am on my the home page page$/,
+    async () => {
+        const baseurl = process.env.BASE_URL;
+        if (!baseurl) throw new Error('BASE_URL not defined in enviornment');
+
+        await goToUrl(baseurl);
+    }
+);
+Then(
+    /^there should be no accessibility violations detected$/,
+    async function () {
+    await checkNoAccessibilityViolations();
+});
