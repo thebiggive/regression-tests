14be5fb3dd8c8780c4dc3d867f20084b4e30c659
Barney Laurance <barney@thebiggive.org.uk>
2026-08-03 16:38:56 +0100

    Temporarily stop testing on safari via testingbot
    Not sure why safari isn't working, would like to see if we get to green
    with Chrome and Edge before trying to restore Safari

--- wdio.testingbot-all.conf.cjs
+++ wdio.testingbot-all.conf.cjs
@@ -7,12 +7,13 @@ config.capabilities = [
         version: 'latest',
         build: config.build,
     },
-    {
-        browserName: 'safari',
-        platform: 'VENTURA',
-        version: '16', // 16.3 – older than the point releases which last reached iOS – as of April 2025
-        build: config.build,
-    },
+    // temporarily removed Safari because connections to safari seem to be timing out in testingbot.
+    // {
+    //     browserName: 'safari',
+    //     platform: 'VENTURA',
+    //     version: '16', // 16.3 – older than the point releases which last reached iOS – as of April 2025
+    //     build: config.build,
+    // },
     {
         browserName: 'microsoftedge',
         platform: 'WIN11',
